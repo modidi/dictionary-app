b968dbc3d64697e3af138ee6e21972f919a6de7f
maureen <maureenndindamutua@gmail.com>
2026-07-16 10:41:30 +0300

    feat: implement search functionality and initialize app on page load

--- wordly/js/script.js
+++ wordly/js/script.js
@@ -31,3 +31,46 @@ const emptyFavorites = document.getElementById("empty-favorites");
 
 // Theme button
 const toggleThemeButton = document.getElementById("toggle-theme");
+
+//Wait until the html page has fully loaded
+document.addEventListener("DOMContentLoaded",initializeApp);
+
+function initializeApp(){
+
+    // Listen for form submission
+    searchForm.addEventListener("submit", handleSearch);
+
+    //Listen for theme button clicks
+    toggleThemeButton.addEventListener("click", toggleTheme);
+
+    //Display savedFavorites
+    displayFavorites();
+
+    //Restore the saved theme
+    loadTheme();
+}
+
+// Handle Search Submission
+function handleSearch(event) {
+    // Prevent the browser from refreshing the page
+    event.preventDefault();
+
+    //Remove any previous error message
+    errorMessage.textContent = "";
+
+    //Clear previous search results
+    clearResults();
+
+    //Read the users's input
+    let word = wordInput.value.trim().toLowerCase();
+
+    //Check if the user entered a word
+    if (word === "") {
+        displayError("Please enter a word");
+        return;
+    }
+
+    //fetch the word form the Dictionary API
+    fetchWord(word);
+}
+
